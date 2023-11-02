339e225072181ebd14e2ed554b23639e9f6d0523
andrei andrei <andrei@MacBook-Air-andrei.local>
2023-11-03 00:59:41 +0300

    added yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,28 @@ export default function RootLayout({
 
   return (
     <html lang="ru">
+              <head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          
+              ym(95416777, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true,
+                   webvisor:true
+              });
+              `
+            }}
+          />
+        </head>
       <body className={inter.className}>
+      <noscript><div><img src="https://mc.yandex.ru/watch/95416777" style={{position:"absolute", left:"-9999px"}} alt="" /></div></noscript>
       <Providers>
         <StyledComponentsRegistry>
         <NextTopLoader color='red' />
