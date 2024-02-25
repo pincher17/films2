b67c2e3ca0122cde6750621debbcd53faa30a50b
andrei andrei <andrei@MacBook-Air-andrei.local>
2024-02-25 15:57:03 +0300

    robots disallow

---
+++ src/app/styles/Header.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import Telegram from '../../../public/assets/icons/tg.svg'
+import Link from 'next/link';
+
+
+
+export const TelegramIcon = styled(Telegram)`
+  
+`
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,8 @@ import Logo from '../../../public/assets/img/logo1.svg'
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import { AllFilmsIcon, NavigationItem, StyledLink } from '../MobileNavigation';
+import { TelegramIcon } from '@/app/styles/Header.styles';
 
 const Header: React.FC = () =>{
 
@@ -20,6 +22,11 @@ const Header: React.FC = () =>{
         <Search />
         <nav className={s.nav}>
         <Link href={'/films'} className={`${s.nav_item} ${pathname == "/films" ? s.active : ""}`}>Все фильмы</Link>
+        <NavigationItem $active={(+(pathname === '/films'))}>
+{/*         <StyledLink href="https://t.me/filmhub_lol">
+        <TelegramIcon />
+        </StyledLink> */}
+      </NavigationItem>
         </nav>
         </div>
       </header>
