be742942afea79781d39f7cd3301f03dd30d99fa
andrei andrei <andrei@MacBook-Air-andrei.local>
2024-07-04 14:37:35 +0300

    api

--- src/api/apiV4.js
+++ src/api/apiV4.js
@@ -5,7 +5,7 @@ export const instance = axios.create({
     baseURL: 'https://api.kinopoisk.dev/v1.4/',
     headers: {
         'accept': 'application/json',
-        'X-API-KEY': 'K5PMTVP-KZ9MAA5-K088YXZ-YZ51Q77',
+        'X-API-KEY': '',
       }
 })
 
